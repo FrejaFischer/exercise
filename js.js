3833a450a7fcf545c739c51451b141082e3dd3c8
Freja Fischer <frej657e@stud.kea.dk>
2023-10-27 12:40:36 +0200

    Finish

--- js.js
+++ js.js
@@ -85,3 +85,110 @@ searchengines.forEach((link) => {
 const newArr = searchengines.map((link) => `<a href="${link.target}">${link.name}</a>`);
 
 console.log(newArr);
+
+//Øvelse 5 Follow instructions
+const names = ["Jonas", "Dannie", "Peter", "Klaus"];
+/* start by making a "bad" copy of this array 
+by writing something like const myBadCopy = names;
+then modify either array by changing one name
+then console log both, what happened
+*/
+
+// myBadCopy = names;
+// myBadCopy[0] = "Freja";
+// console.log(names); //Den originale er også ændret :-(
+
+/* Then create a copy using the ... spread operator
+modify either array and console log them, what happens now?
+*/
+
+myGoodCopy = [...names];
+console.log(myGoodCopy); //En kopi af names
+myGoodCopy[0] = "Freja";
+console.log(names); //Har ikke ændret sig. Stadig Jonas.
+
+//Øvelse 6 Follow instructions
+const me = {
+  name: "Jonas",
+  age: 45,
+  kids: 3,
+};
+
+/* start by making a bad copy (reference) with something like
+const myCopy = me;
+change something, see what happens */
+// badCopy = me;
+// badCopy.name = "Freja";
+// console.log(badCopy);
+// console.log(me); //Den er også blevet ændret
+/*
+Then make a new copy using the spead operator, 
+change something and see what happens
+*/
+goodCopy = { ...me };
+goodCopy.name = "Freja";
+console.log(goodCopy);
+console.log(me); //Me er ikke blevet ændret
+
+//Øvelse 7 Se video
+//Øvelse 8
+/* this function receives an object with stuff it does not need
+	make it easier for the next developer by destructuring out the 
+	parts we need 
+	Then clean up the function by removing "person.x". */
+
+// function personCard(person) {
+//   return `<div class="person">
+//         <h2>${person.name}</h2>
+//         <ul>
+//             <li><a href="tel:+${person.phone}">Call</a></li>
+//             <li><a href="mailto:${person.email}">E-mail</a></li>
+//         </ul>
+//     </div>`;
+// }
+// const myPerson = {
+//   name: "Clark Kent",
+//   marriage: null,
+//   glasses: true,
+//   phone: 911,
+//   street: "secret",
+//   email: "clark@dailyplanet.com",
+// };
+// personCard(myPerson);
+
+function personCard({ name, phone, email }) {
+  return `<div class="person">
+	        <h2>${name}</h2>
+	        <ul>
+	            <li><a href="tel:+${phone}">Call</a></li>
+	            <li><a href="mailto:${email}">E-mail</a></li>
+	        </ul>
+	    </div>`;
+}
+const myPerson = {
+  name: "Clark Kent",
+  marriage: null,
+  glasses: true,
+  phone: 911,
+  street: "secret",
+  email: "clark@dailyplanet.com",
+};
+console.log(personCard(myPerson));
+
+//Øvelse 9 Follow instruction
+/* this function return 4 random numbers in an array. Use destructuring 
+	to pick out the two first items */
+
+function getNums() {
+  return [Math.random(), Math.random(), Math.random(), Math.random()];
+}
+//Vis den skulle blive til arrow
+// const getNums2=()=>[Math.random(), Math.random(), Math.random(), Math.random()];
+
+const [firstRan, secondRan] = getNums();
+
+function test() {
+  return [1, 2, 3, 4];
+}
+const [firstNum, secondNum] = test();
+console.log(firstNum, secondNum);
